b91adf5250945f52957fc379ba28e67de65f293e
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-08-14 01:19:14 +0530

    Add files via upload

---
+++ Tree/binary_tree_to_sum_tree.cpp
@@ -0,0 +1,82 @@
+// C++ program to convert a tree into its sum tree
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A tree node structure */
+class node
+{
+	public:
+int data;
+node *left;
+node *right;
+};
+
+// Convert a given tree to a tree where
+// every node contains sum of values of
+// nodes in left and right subtrees in the original tree
+int toSumTree(node *Node)
+{
+	// Base case
+	if(Node == NULL)
+	return 0;
+
+	// Store the old value
+	int old_val = Node->data;
+
+	// Recursively call for left and
+	// right subtrees and store the sum as
+	// old value of this node
+	Node->data = toSumTree(Node->left) + toSumTree(Node->right);
+
+	// Return the sum of values of nodes
+	// in left and right subtrees and
+	// old_value of this node
+	return Node->data + old_val;
+}
+
+// A utility function to print
+// inorder traversal of a Binary Tree
+void printPreorder(node* Node)
+{
+	if (Node == NULL)
+		return;
+        cout<<Node->data<<" ";
+	printPreorder(Node->left);
+	printPreorder(Node->right);
+}
+
+/* Utility function to create a new Binary Tree node */
+node* newNode(int data)
+{
+	node *temp = new node;
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+	
+	return temp;
+}
+
+/* Driver code */
+int main()
+{
+	node *root = NULL;
+	
+	/* Constructing tree given in the above figure */
+	root = newNode(10);
+	root->left = newNode(-2);
+	root->right = newNode(6);
+	root->left->left = newNode(8);
+	root->left->right = newNode(-4);
+	root->right->left = newNode(7);
+	root->right->right = newNode(5);
+	
+	toSumTree(root);
+	
+	// Print inorder traversal of the converted
+	// tree to test result of toSumTree()
+	cout<<"Preorder Traversal of the resultant tree is: \n";
+	printPreorder(root);
+	return 0;
+}
+
+// This code is contributed by rathbhupendra
